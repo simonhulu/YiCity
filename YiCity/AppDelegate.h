d3fd9b189ccfd1ba1f2b41f833b5c79e9e4568cb
james <tmyk104117@126.com>
2015-11-21 11:04:53 +0800

    Initial Commit

---
+++ YiCity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YiCity
+//
+//  Created by qizhang on 11/21/15.
+//  Copyright (c) 2015 hezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YiCity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YiCity
+//
+//  Created by qizhang on 11/21/15.
+//  Copyright (c) 2015 hezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
