0469960108fa1945f5944676ea05cc8bed9a65b8
james <tmyk104117@126.com>
2015-12-03 20:54:12 +0800

    asdasd

--- YiCity/YCManager/YCShareManager/QQShareManager.m
+++ YiCity/YCManager/YCShareManager/QQShareManager.m
@@ -67,4 +67,38 @@ static QQShareManager *singleton = nil ;
     return req ;
 }
 
+-(BOOL)login
+{
+    NSArray* permissions = [NSArray arrayWithObjects:
+                            kOPEN_PERMISSION_GET_USER_INFO,
+                            kOPEN_PERMISSION_GET_SIMPLE_USER_INFO,
+                            kOPEN_PERMISSION_ADD_ALBUM,
+                            kOPEN_PERMISSION_ADD_IDOL,
+                            kOPEN_PERMISSION_ADD_ONE_BLOG,
+                            kOPEN_PERMISSION_ADD_PIC_T,
+                            kOPEN_PERMISSION_ADD_SHARE,
+                            kOPEN_PERMISSION_ADD_TOPIC,
+                            kOPEN_PERMISSION_CHECK_PAGE_FANS,
+                            kOPEN_PERMISSION_DEL_IDOL,
+                            kOPEN_PERMISSION_DEL_T,
+                            kOPEN_PERMISSION_GET_FANSLIST,
+                            kOPEN_PERMISSION_GET_IDOLLIST,
+                            kOPEN_PERMISSION_GET_INFO,
+                            kOPEN_PERMISSION_GET_OTHER_INFO,
+                            kOPEN_PERMISSION_GET_REPOST_LIST,
+                            kOPEN_PERMISSION_LIST_ALBUM,
+                            kOPEN_PERMISSION_UPLOAD_PIC,
+                            kOPEN_PERMISSION_GET_VIP_INFO,
+                            kOPEN_PERMISSION_GET_VIP_RICH_INFO,
+                            kOPEN_PERMISSION_GET_INTIMATE_FRIENDS_WEIBO,
+                            kOPEN_PERMISSION_MATCH_NICK_TIPS_WEIBO,
+                            nil];
+    return [_tencentOAuth authorize:permissions inSafari:NO] ;
+}
+
+-(void)tencentDidLogin
+{
+    NSLog(@"%@",_tencentOAuth.openId) ;
+}
+
 @end
