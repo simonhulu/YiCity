df9c3d62e23d5f2de8f38f1405248c4b05ee65ba
james <tmyk104117@126.com>
2015-12-12 16:31:51 +0800

    fix bugs

---
+++ YiCity/YCControls/YCShareView.h
@@ -0,0 +1,13 @@
+//
+//  YCShareVIew.h
+//  YiCity
+//
+//  Created by qizhang on 12/12/15.
+//  Copyright (c) 2015 hezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YCShareVIew : UIView
+
+@end
---
+++ YiCity/YCControls/YCShareView.m
@@ -0,0 +1,21 @@
+//
+//  YCShareVIew.m
+//  YiCity
+//
+//  Created by qizhang on 12/12/15.
+//  Copyright (c) 2015 hezi. All rights reserved.
+//
+
+#import "YCShareVIew.h"
+
+@implementation YCShareVIew
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
